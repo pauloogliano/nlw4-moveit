52a11e3b1728a4f73f95e71a9672f78b9dd19248
Paulo Ogliano <contato@pauloogliano.com>
2021-02-25 21:51:44 +0000

    Day 4

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -3,7 +3,6 @@ import '../styles/global.css';
 import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
-  
 
   return (
     <ChallengesProvider>
